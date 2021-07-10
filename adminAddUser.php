7cf76546d51e79683f8ba842d0f51e3c3affb8c2
Omar Khater <40499227+omar0alaa@users.noreply.github.com>
2021-07-10 21:23:00 +0800

    Update adminAddUser.php

--- adminAddUser.php
+++ adminAddUser.php
@@ -22,7 +22,7 @@ if ($_SESSION["LEVEL"] == "Admin"){ ?>
 <div class="sidebar">
 
     <div class="top-icon">
-        <a href="#"><span><i class="fas fa-user"></i></span></a>
+        <a href="My Profile.php"><span><i class="fas fa-user"></i></span></a>
         <a href="#"><span><i class="fas fa-bars"></i></span></a>
         <a href="#"><span><i class="fas fa-cog"></i></span></a>
     </div>
